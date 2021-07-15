40e71dc048e846d87cb94ee7b154a47989860b32
Dmitry Gaiduk <gaidukdg@yandex.ru>
2021-07-15 12:44:42 +0300

    Homework is completed - raw version

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println(transfer(cardType = "Maestro", currentTransfer = 10_000_00))
+}
+
+fun transfer(cardType: String = "VK Pay", previousTransfers: Int = 100_000_00, currentTransfer: Int): Int {
+    val feeInPercent = 0.0075
+    val feeInPercentMaestro = 0.006
+    val floatingFeeMaestro = (currentTransfer * feeInPercentMaestro).toInt() + 2000
+    val minimalFee = 3500
+    val floatingFee = (currentTransfer * feeInPercent).toInt()
+    var totalFee = 0
+    when (cardType) {
+        "VK Pay" -> totalFee = 0
+        "Visa", "Мир" -> totalFee = if (floatingFee <= minimalFee) minimalFee else floatingFee
+        "MasterCard", "Maestro" -> totalFee = if (previousTransfers <= 75_000_00) 0 else floatingFeeMaestro
+    }
+    return totalFee
+}
